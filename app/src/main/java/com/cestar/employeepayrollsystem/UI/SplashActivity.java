ccc8036b6b93525d433762fbcb99cc6113f34076
Ramanpreet Singh <ramanpreet230@gmail.com>
2019-12-06 04:50:39 -0500

    Added detail splash activity.

--- app/src/main/java/com/cestar/employeepayrollsystem/UI/SplashActivity.java
+++ app/src/main/java/com/cestar/employeepayrollsystem/UI/SplashActivity.java
@@ -4,24 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-
-import com.cestar.employeepayrollsystem.MainActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private final int SPLASH_DISPLAY_DURATION = 5000;
-
     @Override
-    public void onCreate(Bundle bundle) {
-        super.onCreate(bundle);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
-                SplashActivity.this.startActivity(mainIntent);
-                SplashActivity.this.finish();
-            }
-        }, SPLASH_DISPLAY_DURATION);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(SplashActivity.this, DetailSplashActivity.class));
+        finish();
     }
 }
