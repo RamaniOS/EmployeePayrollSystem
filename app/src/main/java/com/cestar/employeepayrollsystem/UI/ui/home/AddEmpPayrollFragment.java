5c9d6d28d8113670c7179bd32a85a2b22e31e1cc
Nitin Jaswal <nathanjaswal@gmail.com>
2019-12-11 02:59:36 -0500

    [Nitin] code updated

--- app/src/main/java/com/cestar/employeepayrollsystem/UI/ui/home/AddEmpPayrollFragment.java
+++ app/src/main/java/com/cestar/employeepayrollsystem/UI/ui/home/AddEmpPayrollFragment.java
@@ -135,7 +135,7 @@ public class AddEmpPayrollFragment extends Fragment {
         schoolET.setText("");
         salaryET.setText("");
         bonusET.setText("");
-        //////////////////////////
+        ////////////////////////////
 
     }
 
